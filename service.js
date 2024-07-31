a68f19cf47cfbe0a3813e0c3f2ee3f7da07b17f4
luisrjr <65251650+luisrjr@users.noreply.github.com>
2024-07-30 23:01:40 -0700

    Removed service class

--- service.js
+++
@@ -1,9 +0,0 @@
-const Service = class {
-  constructor(category, mileage, description) {
-    this.category = category;
-    this.mileage = mileage;
-    this.description = description;
-  }
-};
-
-export default Service;
