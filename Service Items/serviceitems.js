7123714097e53069d6d89fcdbc8cf18fdc4fc1c3
luisrjr <65251650+luisrjr@users.noreply.github.com>
2024-08-23 21:08:13 -0700

    Started Module Building

--- Service Items/serviceitems.js
+++ Service Items/serviceitems.js
@@ -8,9 +8,10 @@ const addServiceBtn = document.getElementById("addbtn");
 const formBody = document.getElementById("add-service-form-body");
 const insertServiceBtn = document.getElementById("insert-service-btn");
 const cancelServiceBtn = document.getElementById("cancel-service-btn");
+const deleteServiceBtn = document.getElementById("delete-service-btn");
+const editServiceBtn = document.getElementById("edit-service-btn");
 
-const serviceData = JSON.parse(localStorage.getItem("data")) || [];
-let currentService = {};
+const serviceData = JSON.parse(localStorage.getItem("maintenance_data")) || [];
 
 const insertOrUpdateServiceItem = () => {
   const serviceObj = {
@@ -29,11 +30,49 @@ const insertOrUpdateServiceItem = () => {
     serviceData.unshift(serviceObj);
   } else {
     serviceData[serviceItemsIndex] = serviceObj;
-  };
-  
+  }
 
-  localStorage.setItem("data", JSON.stringify(serviceData));
+  localStorage.setItem("maintenance_data", JSON.stringify(serviceData));
   displayService();
+
+  formBody.classList.add("hidden");
+};
+
+const deleteServiceItem = () => {
+  console.log("clicked");
+  // const serviceItemsIndex = serviceData.findIndex(
+  //   (item) => item.id === serviceObj.id
+  // );
+
+  // if (serviceItemsIndex === -1) {
+  //   return;
+  // } else {
+  //   serviceData.splice(serviceItemsIndex, 1); //[serviceItemsIndex] = ;
+  // }
+};
+
+const displayService = () => {
+  displayContainer.innerHTML = "";
+  serviceData.forEach((serviceObject) => {
+    displayContainer.innerHTML += `
+        <div class="task" id="${serviceObject.id}">${serviceObject.id}
+        <p><strong>Mileage:</strong> ${serviceObject.mileage}</p>
+          <p><strong>Category:</strong> ${serviceObject.category}</p>
+          <p><strong>Description:</strong> ${serviceObject.description}</p>
+          <p><strong>Date:</strong> ${serviceObject.dateOfService}</p>
+          <button onclick="editServiceItem()" id = "edit-service-btn" type="button" class="btn">Edit</button>
+          <button onclick="deleteServiceItem()" id = "delete-service-btn" type="button" class="btn">Delete</button> 
+        </div>
+      `;
+  });
+};
+
+const resetForm = () => {
+  serviceCategory.value = "";
+  dateOfService.value = "";
+  serviceCategory.value = "";
+  descriptionInput.value = "";
+  mileageInput.value = "";
 
   formBody.classList.add("hidden");
 };
@@ -52,27 +91,7 @@ insertServiceBtn.addEventListener("click", (event) => {
   resetForm();
 });
 
-const displayService = () => {
-  displayContainer.innerHTML = "";
-  serviceData.forEach((serviceObject) => {
-    displayContainer.innerHTML += `
-        <div class="task" id="${serviceObject.id}">
-        <p><strong>Mileage:</strong> ${serviceObject.mileage}</p>
-          <p><strong>Category:</strong> ${serviceObject.category}</p>
-          <p><strong>Description:</strong> ${serviceObject.description}</p>
-          <p><strong>Date:</strong> ${serviceObject.dateOfService}</p>
-          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
-          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
-        </div>
-      `;
-  });
-};
-
-const resetForm = () => {
-  serviceCategory.value = "";
-  dateOfService.value = "";
-  serviceCategory.value = "";
-  descriptionInput.value = "";
-  mileageInput.value = "";
-  formBody.classList.add("hidden");
-};
+deleteServiceBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  deleteServiceItem()
+});
--- garage/garage.js
+++ garage/garage.js
@@ -3,8 +3,8 @@ let garage = [];
 const makeInput = document.getElementById("make-input");
 const modelInput = document.getElementById("model-input");
 const addVehBtn = document.getElementById("addvehiclebtn");
-
 let dateDropdown = document.getElementById("date-dropdown");
+const garageData = JSON.parse(localStorage.getItem("garage_data")) || [];
 
 let currentYear = new Date().getFullYear();
 let earliestYear = 1970;
@@ -16,14 +16,31 @@ while (currentYear >= earliestYear) {
   currentYear -= 1;
 }
 
-const addVehicle = () => {
-  vehicle = {
+const insertOrUpdateGarageVehicle = () => {
+  vehicleObj = {
     carid: `${makeInput.value}${modelInput.value}${Date.now()}`,
     year: dateDropdown.value,
     make: makeInput.value,
     model: modelInput.value,
   };
-  garage.push(vehicle);
+
+  const vehicleIndex = garageData.findIndex(
+    (car) => car.id === vehicleObj.carid
+  );
+
+  if (vehicleIndex === -1) {
+    garageData.unshift(vehicleObj);
+  } else {
+    garageData[vehicleIndex] = vehicleObj;
+  }
+
+  localStorage.setItem("garage_data", JSON.stringify(garageData));
+  displayService();
+
+  //formBody.classList.add("hidden");
 };
 
-addVehBtn.addEventListener("click", addVehicle);
+addVehBtn.addEventListener("click", insertOrUpdateGarageVehicle);
+currentYear;
+
+export default garage;
